d14bc3ee26e3e57af7f4999cfec3a82843bc194c
gaussberg <gauss.berg@foxmail.com>
2020-09-04 09:48:13 +0800

    后端

---
+++ user/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    #http://127.0.0.1:8000/v1/users/activation
+    path('activation', views.active_view),
+    #/v1/users/<username>/address
+    path('<str:username>/address', views.AddressView.as_view()),
+    path('findPass', views.find_pass_view),
+    path('update', views.update_view),
+    path('modify', views.modify_view),
+
+    path('weibo/authorization', views.weibo_url_view),
+    path('weibo/users', views.WeiboUserView.as_view())
+
+]
+
+
+
+
